1abf97bc0db592898a5ac8e608b3056a76443778
James Moore <jamesmoore@rocketinnovationstudio.ca>
2021-11-25 10:43:42 -0500

    POST, PUSH, Delete ItemsController implementations

--- Repositories/IItemsRepository.cs
+++ Repositories/IItemsRepository.cs
@@ -11,5 +11,7 @@ namespace CatalogApi.Repositories
         Item GetItem(Guid id);
         IEnumerable<Item> GetItems();
         void CreateItem(Item item);
+        void UpdateItem(Item item);
+        void DeleteItem(Guid id);
     }
 }
