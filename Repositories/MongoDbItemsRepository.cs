e41a8cb241ebe72ba757324f016c96d55b873bd8
James Moore <jamesmoore@rocketinnovationstudio.ca>
2021-11-29 20:28:27 -0500

    added rest of repository methods update delete find findal

--- Repositories/MongoDbItemsRepository.cs
+++ Repositories/MongoDbItemsRepository.cs
@@ -13,6 +13,7 @@ namespace CatalogApi.Repositories
         private const string databaseName = "catalog";
         private const string collectionName = "items";
         private readonly IMongoCollection<Item> itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
         
         public MongoDbItemsRepository(IMongoClient mongoClient)
         {
@@ -27,16 +28,21 @@ namespace CatalogApi.Repositories
 
         public void UpdateItem(Item item)
         {
-            
+            var filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
+
+            itemsCollection.ReplaceOne(filter, item);
         }
         public void DeleteItem(Guid id)
         {
-            
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            itemsCollection.DeleteOne(filter);
         }
 
         public Item GetItem(Guid id) 
         {
-            return new Item();
+            var filter = filterBuilder.Eq(item => item.Id, id);
+
+            return itemsCollection.Find(filter).SingleOrDefault();
         }
 
         public IEnumerable<Item> GetItems() 
