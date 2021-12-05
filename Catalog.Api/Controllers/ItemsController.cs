be62e5875642e42d861d9fd20a04472e75ca1137
James Moore <jamesmoore@rocketinnovationstudio.ca>
2021-12-05 15:10:38 -0500

    6:10 Refactored dtos from records to classes and put into one single file, refactored tests to now work with classes not records. removed some record specific conversions

--- Catalog.Api/Controllers/ItemsController.cs
+++ Catalog.Api/Controllers/ItemsController.cs
@@ -49,6 +49,7 @@ namespace Catalog.Api.Controllers
             {
                 Id = Guid.NewGuid(),
                 Name = itemDto.Name,
+                Description = itemDto.Description,
                 Price = itemDto.Price,
                 CreatedDate = DateTimeOffset.UtcNow
             };
@@ -67,12 +68,10 @@ namespace Catalog.Api.Controllers
                 return NotFound();
             }
 
-            Item updatedItem = existingItem with {
-                Name = itemDto.Name,
-                Price = itemDto.Price
-            };
+            existingItem.Name = itemDto.Name;
+            existingItem.Price = itemDto.Price;
 
-            await repository.UpdateItemAsync(updatedItem);
+            await repository.UpdateItemAsync(existingItem);
 
             return NoContent();
         }
--- Catalog.Api/Models/Item.cs
+++ Catalog.Api/Models/Item.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 
 namespace Catalog.Api.Models
 {
-    public record Item
+    public class Item
     {
-        public Guid Id { get; init; }
-        public string Name { get; init; }
-        public decimal Price { get; init; }
-        public DateTimeOffset CreatedDate { get; init; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
     }
 }
 // records are immutable objects
